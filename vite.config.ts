6b0490e4c2b7d0bec2a22993a4f8198d0bee7152
sayyadkhalidhussain748-lgtm <sayyadkhalidhussain748@gmail.com>
2026-04-08 14:02:02 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      // Add this line so assets load correctly on WordPress
+      base: './', 
       server: {
         port: 3000,
         host: '0.0.0.0',
